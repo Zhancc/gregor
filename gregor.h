e583aa3c0ee6905bfcbf1da8b1375114356b1ea2
zhanc1 <zhanc1@andrew.cmu.edu>
2016-05-07 02:03:58 -0400

    create a src directory

--- gregor.h
+++
@@ -1,8 +0,0 @@
-#ifndef GREGOR_H
-#define GREGOR_H
-
-#include "thread.h"
-int spawn(enum type rt,void* return_ptr, void* routine, int num_arg, ...);
-int __gregor_sync();
-void set_next_job(jcb* job);
-#endif
